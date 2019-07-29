7e8c42847812cbd6cd54df7a13173b15fa559c08
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-07-29 21:37:31 +0100

    Simple switch statement

---
+++ switch.js
@@ -0,0 +1,52 @@
+switch (choice) {
+	case "January":
+		days = 31;
+		break;
+
+	case "February":
+		days = 28;
+		break;
+
+	case "March":
+		days = 31;
+		break;
+
+	case "April":
+		days = 30;
+		break;
+
+	case "May":
+		days = 31;
+		break;
+
+	case "June":
+		days = 30;
+		break;
+
+	case "July":
+		days = 31;
+		break;
+
+	case "August":
+		days = 31;
+		break;
+
+	case "September":
+		days = 30;
+		break;
+
+	case "October":
+		days = 31;
+		break;
+
+	case "November":
+		days = 30;
+		break;
+
+	case "December":
+		days = 30;
+		break;
+
+	default:
+		days = 30;
+}
